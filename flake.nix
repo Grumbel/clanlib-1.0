5f020d2c7daed3171dffa6dc88a9d821b95549a9
Ingo Ruhnke <grumbel@gmail.com>
2021-11-20 20:33:35 +0100

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,50 @@
+rec {
+  description = "ClanLib game SDK development files";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          clanlib = pkgs.stdenv.mkDerivation {
+            pname = "clanlib";
+            version = "1.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            enableParallelBuilding = true;
+            preConfigure = "./autogen.sh";
+            nativeBuildInputs = [
+              pkgs.autoconf
+              pkgs.automake
+              pkgs.gcc
+              pkgs.libtool
+              pkgs.perl
+              pkgs.pkgconfig
+            ];
+            buildInputs = [
+              pkgs.SDL
+              pkgs.SDL_gfx
+              pkgs.alsa-lib
+              pkgs.libGL
+              pkgs.libGLU
+              pkgs.libjpeg
+              pkgs.libmikmod
+              pkgs.libogg
+              pkgs.libpng
+              pkgs.libvorbis
+              pkgs.libvorbis
+              pkgs.xorg.libXi
+              pkgs.xorg.libXmu
+              pkgs.xorg.libXxf86vm
+            ];
+           };
+        };
+        defaultPackage = packages.clanlib;
+      });
+}
